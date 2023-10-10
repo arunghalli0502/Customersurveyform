372e7d0edfae49c9bf2e69f4bd47b9832cfac6ca
Arun <121216673+arunghalli0502@users.noreply.github.com>
2023-10-11 00:56:26 +0530

    Update Surveyform.js

--- Surveyform.js
+++ Surveyform.js
@@ -7,11 +7,17 @@ let suggestion = document.getElementById("suggestions");
 let errormessage = document.getElementById("errormessage");
 let datadisppopup = document.querySelectorAll(".datadisppopup");
 let datadisppopup1 = document.getElementById("datadisppopup");
+let Proffesion = document.getElementById("Proffesion");
+let email = document.getElementById("email");
+let mobile = document.getElementById("mobile");
+let dob = document.getElementById("dob");
+let gender= document.getElementsByName("gender");
+let country= document.getElementById("country");
 const regex = /^[a-zA-Z-'\s]+$/;
 function submitsurveydata() 
 {
     event.preventDefault();
-    if( validationform(fname)  && validationform(lname)  && validateoptions(firsttime) && validateoptions(suggested) && validateoptions(rating)  && validationform(suggestion) )
+    if( validationform(fname)  && validationform(lname) && validationform(Proffesion) && validateoptions(gender) && validateoptions(firsttime)  && validateoptions(suggested) && validateoptions(rating)  && validationform(suggestion))
     {
         alert("Your response saved successfully")
         datadisppopup[0].children[2].innerText="First Name : "+fname;
@@ -19,7 +25,12 @@ function submitsurveydata()
         datadisppopup[0].children[4].innerText="Is this the first time your are using our product and service? : "+firsttime;
         datadisppopup[0].children[5].innerText="Would you suggestion to your friend and colleague? : "+suggested;
         datadisppopup[0].children[6].innerText="How satisfied are you with our comapny overall? : "+rating;
-        datadisppopup[0].children[7].innerText="Do you have any suggestion to improve our service? : "+suggestion;
+        datadisppopup[0].children[7].innerText="Gender : "+gender;
+        datadisppopup[0].children[8].innerText="Email : "+email.value;
+        datadisppopup[0].children[9].innerText="Proffesion : "+Proffesion;
+        datadisppopup[0].children[10].innerText="Mobile : " + country.value+mobile.value;
+        datadisppopup[0].children[11].innerText="DOB : "+dob.value;
+        // datadisppopup[0].children[12].innerText="Country : "+country.value;
         datadisppopup1.style.display="block";
         return true;
     }
@@ -42,6 +53,10 @@ function validationform(inputfieldvalues)
         {
             lname= inputfieldvalues.value;
         }
+        else if(inputfieldvalues.id == "Proffesion")
+        {
+          Proffesion= inputfieldvalues.value;
+        }
         else
         {
             suggestion= inputfieldvalues.value;
@@ -58,6 +73,10 @@ function validationform(inputfieldvalues)
        else if(inputfieldvalues.id == "lname")
        {
             errormessage.innerText= "Please enter a valid last name."
+       }
+        else if(inputfieldvalues.id == "Proffesion")
+       {
+            errormessage.innerText= "Please enter a valid proffesion";
        }
        else
        {
@@ -81,6 +100,11 @@ function  validateoptions(k)
           else if(k[i].id === "suggested")
           {
             suggested = k[i].value;
+            return true;
+          }
+          else if(k[i].id === "gender")
+          {
+            gender = k[i].value;
             return true;
           }
           else
